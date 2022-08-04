82e1964c9d184829872d05868ed5230d93c6ed3c
AhmedSharafEldin2 <20P3945@eng.asu.edu.eg>
2022-08-04 21:17:22 +0200

    3rd version

--- Sum2Numbers/src/main/kotlin/MainPackage/main.kt
+++ Sum2Numbers/src/main/kotlin/MainPackage/main.kt
@@ -1,8 +1,8 @@
 package MainPackage
 
 fun main(args : Array <String>){
-    val number1 : Double = 10.0
-    val number2 : Double = 9.0
+    val number1 : Double = 16.0
+    val number2 : Double = 2.0
     val result = number1 + number2
     print(result)
 }
